10794557aea8b2e921a5cb06d01414310be0c5c5
Abishek A <126270342+AbiMoni2003@users.noreply.github.com>
2024-11-28 13:26:47 +0530

    login validate

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,9 +1,23 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import logo from '../assets/CFlogo.png'
 import '../login/Logi-Style.css';
 import {Link} from 'react-router-dom';
 
 function Login() {
+  const [username,setUsername]=useState("");
+  const [password,setPassword]=useState("");
+
+  const loginvalidate=()=>{
+    if(username==""){
+      alert("Username is Empty");
+    }
+    else if(password==""){
+      alert("Password is Empty")
+    }
+    else{
+      
+    }
+  }
   return (
     <Fragment>
        <header>
@@ -12,10 +26,10 @@ function Login() {
        </header>
        <div className='login-top-div'>
         <h1>Login</h1>
-        <input type="text"  placeholder='👤 username'  />
-        <input type="password" placeholder='🔐 password'  />
+        <input type="text"  placeholder='👤 username'  value={username} onChange={(e)=>setUsername(e.target.value)}/>
+        <input type="password" placeholder='🔐 password' value={password} onChange={(e)=>setPassword(e.target.value)} />
         <p className='login-forgot'><a><u><Link to="/forgot">forgot password?</Link></u></a></p>
-        <button className='login-btn'>Login</button>
+        <button className='login-btn' onClick={loginvalidate}>Login</button>
         <p className='login-dont'>Don't have an account?<a><u><Link to="/register"> Sign up</Link> </u></a></p>
        </div>
     </Fragment>
