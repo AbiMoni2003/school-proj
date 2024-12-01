9ab030526f0c7cd7bb529f429b5c0be1f55995d5
Abishek A <126270342+AbiMoni2003@users.noreply.github.com>
2024-12-01 17:05:33 +0530

    added

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,24 +1,43 @@
-import React, { Fragment } from 'react'
+import React, { Fragment,useState } from 'react'
 import logo from '../assets/CFlogo.png'
 import { Link } from 'react-router-dom'
 
 function Home() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <Fragment>
-        <div className='whole-div'>
-          <header className='home-header'>
-            <nav>
-                <ul>
-                    <li><img src={logo} alt="School Logo" className="header-logo" /></li>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                   <Link to="/register"><li><button className='home-btn'>SignUp</button></li></Link> 
-                  <Link to="/login" className='link2'> <li><button className="home-btn">Login</button></li></Link>
-                </ul>
-            </nav>
-          </header>
+      <div className='whole-div'>
+        <header className='home-header'>
+          <nav>
+            <img src={logo} alt='Company Logo' className='header-logo' />
+            <ul className={`nav-list ${isMenuOpen ? 'open' : ''}`}>
+              <li className="nav-item">Home</li>
+              <li className="nav-item">About</li>
+              <li className="nav-item">Contact</li>
+            </ul>
+            <div>
+              <Link to="/register">
+                <button className="home-btn">Sign Up</button>
+              </Link>
+              <Link to="/login">
+                <button className="home-btn">Login</button>
+              </Link>
+            </div>
+            <input type='checkbox' id='check' checked={isMenuOpen} onChange={toggleMenu} />
+            <label htmlFor='check' className='check-box' onClick={toggleMenu}>
+              <i className='fas fa-bars'></i>
+            </label>
+          </nav>
+        </header>
+        <div>
+          {/* Additional content can go here */}
         </div>
+      </div>
     </Fragment>
   )
 }
